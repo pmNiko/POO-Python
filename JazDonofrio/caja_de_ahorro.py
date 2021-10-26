bd84cf997d0c20c43e5ada5abb69003d2d0f4dbc
JazDonofrio <jazmindonofrio@gmail.com>
2021-10-26 20:32:43 -0300

    Caja de ahorro

---
+++ JazDonofrio/caja_de_ahorro.py
@@ -0,0 +1,18 @@
+"""
+    Ejercicio 1:
+        Modelar el objeto caja de ahorro:
+            - un titular
+            - un saldo
+        
+        y debera poder:
+            - depositar un monto
+            - puedeExtraer un monto
+            - extraer un monto
+            - en el caso de querer extraer un monto superior 
+              al monto disponible arrojara una 
+              excepcion ValueError("Imposible realizar extracción.")
+
+        Realizar los test necesarios que validen 
+        el comportamiento.  
+"""
+print("Hola")
